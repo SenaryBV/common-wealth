af7faf65ec7725635189c5a633aef18cc93559b8
alexey <alexey.pytalev@gmail.com>
2021-06-20 15:19:35 +0300

    Revert "fix console errors"

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -15,11 +15,11 @@ const Footer = ({ siteTitle }) => {
     <Container as="footer" className="footer">
       <div className="footer__section footer__section--top">
         <Logo title={siteTitle} />
-        {!isMobile && <Nav nav={FOOTER_NAV} modifier="header" id="footer" />}
+        {!isMobile && <Nav nav={FOOTER_NAV} modifier="header" />}
         <MemberPortal />
       </div>
       <div className="footer__section footer__section--bottom">
-        <Nav nav={FOOTER_COPY} modifier="footer" id="footer-copy" />
+        <Nav nav={FOOTER_COPY} modifier="footer" />
         <SocialLinks links={SOCIAL_LINKS} />
         <div className="footer__copy">© 2021 {siteTitle}. All Rights Reserved</div>
       </div>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 import classNames from 'classnames'
 import { Link } from 'gatsby'
 
-const Nav = ({ nav, id, modifier }) => {
+const Nav = ({ nav, modifier }) => {
   return (
     <ul className={classNames('site-nav', modifier && `site-nav--${modifier}`)}>
       {nav.map(({ name, link }) => (
-        <li key={id + name} className="site-nav__item">
+        <li key={name} className="site-nav__item">
           <Link className="site-nav__link" to={link} activeClassName="is-active">
             {name}
           </Link>
