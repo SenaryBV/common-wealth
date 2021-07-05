d1899a8baf037da6595057ac67f3fdb56dac3520
Dan Halperin <dee.aye.en@gmail.com>
2021-07-05 22:59:21 +0300

    qa hotfixes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Logo from '../Logo'
 import Nav from '../Nav'
 import MemberPortal from '../MemberPortal'
 import SocialLinks from '../SocialLinks'
 import { useMediaQueries } from '../Hooks'
+import ReactTypeformEmbed from 'react-typeform-embed/lib/ReactTypeformEmbed'
+import { isMobile as isMobile2 } from 'react-device-detect'
 
 // constants
 import { FOOTER_NAV, FOOTER_COPY } from '../Nav/constants'
@@ -11,16 +13,29 @@ import { SOCIAL_LINKS } from '../SocialLinks/constants'
 
 const Footer = ({ siteTitle }) => {
   const { isMobile } = useMediaQueries()
+  let typeform = useRef(null)
+  const onContact = () => {
+    console.log('hi')
+
+    isMobile2
+      ? window.open('https://form.typeform.com/to/EjBWyJwr?typeform-medium=embed-snippet', '_blank')
+      : typeform.typeform.open()
+  }
 
   return (
     <footer className="footer container">
+      <ReactTypeformEmbed
+        popup
+        url="https://form.typeform.com/to/EjBWyJwr?typeform-medium=embed-snippet"
+        ref={(tf) => (typeform = tf)}
+      />
       <div className="footer__section footer__section--top">
         <Logo title={siteTitle} />
-        {!isMobile && <Nav nav={FOOTER_NAV} modifier="header" />}
+        {!isMobile && <Nav nav={FOOTER_NAV} modifier="header" onContact={onContact} />}
         <MemberPortal />
       </div>
       <div className="footer__section footer__section--bottom">
-        <Nav nav={FOOTER_COPY} modifier="footer" />
+        <Nav nav={FOOTER_COPY} modifier="footer" onContact={onContact} />
         <SocialLinks links={SOCIAL_LINKS} />
         <div className="footer__copy">© 2021 {siteTitle}. All Rights Reserved</div>
       </div>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,27 +1,14 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import classNames from 'classnames'
 import { Link } from 'gatsby'
-import ReactTypeformEmbed from 'react-typeform-embed/lib/ReactTypeformEmbed'
-import { isMobile } from 'react-device-detect'
 
-const Nav = ({ nav, modifier }) => {
-  let typeform = useRef(null)
-  const onRequestEarly = () =>
-    isMobile
-      ? window.open('https://form.typeform.com/to/EjBWyJwr?typeform-medium=embed-snippet', '_blank')
-      : typeform.typeform.open()
-
+const Nav = ({ nav, modifier, onContact }) => {
   return (
     <ul className={classNames('site-nav', modifier && `site-nav--${modifier}`)}>
-      <ReactTypeformEmbed
-        popup
-        url="https://form.typeform.com/to/EjBWyJwr?typeform-medium=embed-snippet"
-        ref={(tf) => (typeform = tf)}
-      />
       {nav.map(({ name, link }) => (
         <li key={name} className="site-nav__item">
           {name === 'Contact' ? (
-            <div className="site-nav__link" style={{ cursor: 'pointer' }} onClick={onRequestEarly}>
+            <div className="site-nav__link" style={{ cursor: 'pointer' }} onClick={onContact}>
               {name}
             </div>
           ) : (
